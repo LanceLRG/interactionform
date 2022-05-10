10d09df31710e0c56bc69ecaef66ab54e7048477
LanceLRG <69918627+LanceLRG@users.noreply.github.com>
2022-05-10 15:35:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const userAction = async () => {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
         'Authorization' : 'Bearer 00D8c000004uHnW!ARQAQB.pKY3oEx4St43Ma_NbChZ9SPtdSoDY.8af3w110UQy1L2Ml8PI5wT1JwZpqiqeUHCDho2Ontl.V5vb.Xh4xvQ95i_N'
       }
     });
